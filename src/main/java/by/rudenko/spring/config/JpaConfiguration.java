bb379396386747b75b86a51ea9fef3b1ac8bdbd1
Katsiaryna Rudzenka <katestasevich30@gmail.com>
2022-10-12 17:47:02 +0300

    Add Condition

---
+++ src/main/java/by/rudenko/spring/config/JpaConfiguration.java
@@ -0,0 +1,16 @@
+package by.rudenko.spring.config;
+
+import by.rudenko.spring.config.condition.JpaCondition;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Conditional(JpaCondition.class)
+@Configuration
+public class JpaConfiguration {
+    @PostConstruct
+    void init(){
+        System.out.println("Jpa configurational is a=enabled");
+    }
+}
