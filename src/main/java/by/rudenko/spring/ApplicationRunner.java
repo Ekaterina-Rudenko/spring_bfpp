fa46ac364510a44b22c8f91078f056cfc526cf07
Katsiaryna Rudzenka <katestasevich30@gmail.com>
2022-10-13 21:49:50 +0300

    Add yaml and DatabaseProperties class

--- src/main/java/by/rudenko/spring/ApplicationRunner.java
+++ src/main/java/by/rudenko/spring/ApplicationRunner.java
@@ -1,17 +1,23 @@
 package by.rudenko.spring;
 
 import by.rudenko.spring.config.ApplicationConfiguration;
+import by.rudenko.spring.config.DatabaseProperties;
 import by.rudenko.spring.database.pool.ConnectionPool;
 import by.rudenko.spring.database.repository.CrudRepository;
 import by.rudenko.spring.service.CompanyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan //in order not to do @component of databaseproperties class
 public class ApplicationRunner {
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class, args);
         System.out.println(context.getBeanDefinitionCount());
+        System.out.println(context.getBean("pool1"));
+        System.out.println(context.getBean(DatabaseProperties.class));
     }
 }
--- src/main/java/by/rudenko/spring/config/JpaConfiguration.java
+++ src/main/java/by/rudenko/spring/config/JpaConfiguration.java
@@ -1,6 +1,8 @@
 package by.rudenko.spring.config;
 
 import by.rudenko.spring.config.condition.JpaCondition;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,11 @@ import javax.annotation.PostConstruct;
 @Conditional(JpaCondition.class)
 @Configuration
 public class JpaConfiguration {
+/*    @Bean
+    @ConfigurationProperties(prefix="db")
+    public DatabaseProperties databaseProperties(){
+        return new DatabaseProperties();
+    }*/
     @PostConstruct
     void init(){
         System.out.println("Jpa configurational is a=enabled");
