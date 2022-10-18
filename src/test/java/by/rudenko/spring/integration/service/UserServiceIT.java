03da8fc600f63bf7c244dd26974bb72407a4e2f1
Katsiaryna Rudzenka <katestasevich30@gmail.com>
2022-10-18 03:19:41 +0300

    Add test annotation, 1 test context

--- src/test/java/by/rudenko/spring/integration/service/UserServiceIT.java
+++ src/test/java/by/rudenko/spring/integration/service/UserServiceIT.java
@@ -1,15 +1,25 @@
 package by.rudenko.spring.integration.service;
 
+import by.rudenko.spring.database.pool.ConnectionPool;
+import by.rudenko.spring.integration.annotation.IT;
 import by.rudenko.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 public class UserServiceIT {
-    @Autowired
-    private UserService userService;
+
+    private final UserService userService;
+    private final ConnectionPool pool1;
+
 
     @Test
-    void test(){}
+    void test(){
+        System.out.println();
+    }
 }
