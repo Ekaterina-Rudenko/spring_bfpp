90cdaafdce713230d0dd15fcc765306f943b7c4a
Katsiaryna Rudzenka <katestasevich30@gmail.com>
2022-10-18 03:19:58 +0300

    Add test annotation, 1 test context

--- src/test/java/by/rudenko/spring/integration/service/UserServiceIT.java
+++ src/test/java/by/rudenko/spring/integration/service/UserServiceIT.java
@@ -5,10 +5,6 @@ import by.rudenko.spring.integration.annotation.IT;
 import by.rudenko.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 
 @IT
 @RequiredArgsConstructor
