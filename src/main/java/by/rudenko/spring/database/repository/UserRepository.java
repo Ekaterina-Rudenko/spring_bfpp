6851f89b2fc0edd8fe7694fe5a7ac12e35646234
Katsiaryna Rudzenka <katestasevich30@gmail.com>
2022-10-30 06:38:11 +0300

    Slice, Page

--- src/main/java/by/rudenko/spring/database/repository/UserRepository.java
+++ src/main/java/by/rudenko/spring/database/repository/UserRepository.java
@@ -2,7 +2,9 @@ package by.rudenko.spring.database.repository;
 
 import by.rudenko.spring.database.entity.Role;
 import by.rudenko.spring.database.entity.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -35,6 +37,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
 
-    List<User> findAllBy(Pageable pageable);
+    //Collection, Stream
+    //Streamable, Slice, Page
+    @Query(value = "select u from User u",
+    countQuery = "select count(distinct u.firstname) from User u")
+    Page<User> findAllBy(Pageable pageable);
 
 }
