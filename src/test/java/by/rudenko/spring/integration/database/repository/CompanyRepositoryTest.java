608223c48a222246678c3380d605a14f8dcdcb01
Katsiaryna Rudzenka <katestasevich30@gmail.com>
2022-10-30 06:25:42 +0300

    Pageable, Sort, tests

--- src/test/java/by/rudenko/spring/integration/database/repository/CompanyRepositoryTest.java
+++ src/test/java/by/rudenko/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -6,8 +6,6 @@ import by.rudenko.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.Commit;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
--- src/test/java/by/rudenko/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/by/rudenko/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,15 +1,18 @@
 package by.rudenko.spring.integration.database.repository;
 
 import by.rudenko.spring.database.entity.Role;
+import by.rudenko.spring.database.entity.User;
 import by.rudenko.spring.database.repository.UserRepository;
 import by.rudenko.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @IT
 @RequiredArgsConstructor
@@ -18,7 +21,33 @@ class UserRepositoryTest {
     private final UserRepository userRepository;
 
     @Test
-    void checkUpdate(){
+    void checkPageable(){
+        var pageable = PageRequest.of(1, 2, Sort.by("id"));
+        var result = userRepository.findAllBy(pageable);
+        assertThat(result).hasSize(2);
+
+    }
+
+    @Test
+    void checkSort() {
+        var sortBy = Sort.sort(User.class);
+        var sort = sortBy.by(User::getFirstname)
+                .and(sortBy.by(User::getLastname));
+
+        var sortById = Sort.by("firstname").and(Sort.by("lastname"));
+        var allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkFirstTop() {
+        var topUser = userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent(user -> assertEquals(5L, user.getId()));
+    }
+
+    @Test
+    void checkUpdate() {
         var ivan = userRepository.getById(1l);
         assertSame(Role.ADMIN, ivan.getRole());
         ivan.setBirthDate(LocalDate.now());
@@ -32,7 +61,7 @@ class UserRepositoryTest {
     }
 
     @Test
-    void checkQueries(){
+    void checkQueries() {
         var users = userRepository.findAllBy("a", "ov");
         System.out.println(users);
     }
