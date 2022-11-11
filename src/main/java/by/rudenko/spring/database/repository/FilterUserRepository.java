d68f4f91f6c06928113eb66b5c6dd024ca548930
Katsiaryna Rudzenka <katestasevich30@gmail.com>
2022-11-11 22:09:25 +0300

    jdbc

--- src/main/java/by/rudenko/spring/database/repository/FilterUserRepository.java
+++ src/main/java/by/rudenko/spring/database/repository/FilterUserRepository.java
@@ -1,5 +1,6 @@
 package by.rudenko.spring.database.repository;
 
+import by.rudenko.spring.database.entity.Role;
 import by.rudenko.spring.database.entity.User;
 import by.rudenko.spring.dto.PersonalInfo;
 import by.rudenko.spring.dto.UserFilter;
@@ -10,5 +11,5 @@ public interface FilterUserRepository {
     List<User> findAllByFilter(UserFilter filter);
 
     //to get dto. JDBC doesnt use hibrnate and it persist context
-   /* List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);*/
+    List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
 }
